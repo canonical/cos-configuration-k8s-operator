54915ddcdd38fb057c41f204abbc971c8101ce9d
sed-i <82407168+sed-i@users.noreply.github.com>
2022-01-31 09:09:02 -0500

    refactor unit tests a bit

--- tests/unit/charm/test_status_vs_config.py
+++ tests/unit/charm/test_status_vs_config.py
@@ -20,7 +20,7 @@ from charm import COSConfigCharm
 logger = logging.getLogger(__name__)
 
 
-class TestRandomHooks(unittest.TestCase):
+class TestBlockedStatus(unittest.TestCase):
     """Feature: Charm's status should reflect the completeness of the config.
 
     Background: For the git-sync sidecar to run, a mandatory config option is needed: repo's URL.
@@ -63,6 +63,17 @@ class TestRandomHooks(unittest.TestCase):
         finally:
             # cleanup added units to prep for reentry by hypothesis' strategy
             self.harness.cleanup()
+
+
+class TestRandomHooks(unittest.TestCase):
+    """Feature: Charm's status should reflect the completeness of the config.
+
+    Background: For the git-sync sidecar to run, a mandatory config option is needed: repo's URL.
+    As long as it is missing, the charm should be "Blocked".
+    """
+
+    def setUp(self) -> None:
+        self.app_name = "cos-configuration-k8s"
 
     @patch("charm.KubernetesServicePatch", lambda x, y: None)
     @given(
@@ -165,36 +176,6 @@ class TestStatusVsConfig(unittest.TestCase):
         self.git_hash_file_path = os.path.relpath(
             self.harness.charm._git_hash_file_path, self.sandbox.root
         )
-
-    @given(st.booleans(), st.integers(1, 5))
-    def test_unit_is_blocked_if_no_config_provided(self, is_leader, num_units):
-        """Scenario: Unit is deployed without any user-provided config."""
-        # without the try-finally, if any assertion fails, then hypothesis would reenter without
-        # the cleanup, carrying forward the units that were previously added
-        try:
-            self.assertEqual(self.harness.model.app.planned_units(), 1)
-
-            # GIVEN any number of units present
-            for i in range(1, num_units):
-                self.harness.add_relation_unit(self.peer_rel_id, f"{self.app_name}/{i}")
-
-            # AND the current unit could be either a leader or not
-            self.harness.set_leader(is_leader)
-
-            # WHEN no config is provided
-
-            # THEN the unit goes into blocked state
-            self.assertIsInstance(self.harness.charm.unit.status, BlockedStatus)
-
-            # AND pebble plan is empty
-            plan = self.harness.get_container_pebble_plan(self.harness.charm._container_name)
-            self.assertEqual(plan.to_dict(), {})
-
-        finally:
-            # cleanup added units to prep for reentry by hypothesis' strategy
-            self.harness.set_leader(False)
-            for i in reversed(range(1, num_units)):
-                self.harness.remove_relation_unit(self.peer_rel_id, f"{self.app_name}/{i}")
 
     @given(st.booleans(), st.integers(1, 5))
     def test_unit_is_blocked_if_repo_url_provided_but_hash_missing(self, is_leader, num_units):
