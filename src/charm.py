1c1dab74479047fa13cf97097e9984b0b0641247
Luca Bello <luca.bello@canonical.com>
2023-07-10 11:57:27 +0000

    fix static check

--- src/charm.py
+++ src/charm.py
@@ -230,7 +230,7 @@ class COSConfigCharm(CharmBase):
         """
         return bool(self.config.get("git_repo"))
 
-    def _exec_sync_repo(self) -> Tuple[str, str]:
+    def _exec_sync_repo(self) -> Tuple[str, Optional[str]]:
         """Execute the sync command in the workload container.
 
         Raises:
